3bd1233cf90b7b7398ff6d5aa3414a704c19405c
DANIELLE ALBA <danib.sgh@gmail.com>
2019-11-20 19:41:33 +0000

    fuzzy match

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+findMatching = (arr, name) => {
+  return arr.filter(e =>{
+    return e.toLowerCase() == name.toLowerCase()
+  })
+}
+
+function fuzzyMatch(arr, st){
+  return arr.filter(e=>{
+    a = e.toLowerCase()
+    return e.includes(st.toLowerCase())
+  })
+}
+
+
+
+matchName = () => {
+  
+}
